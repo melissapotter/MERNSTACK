b4b468d9d6bb78f7aedfc7a63ae88790b8f22ff2
melissapotter <melissapotter15@gmail.com>
2019-07-28 21:16:50 -0400

    try catch err

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -14,22 +16,35 @@ router.post('/', [
     'password',
     'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const { name, email, password } = req.body;
 
-    // See if user exists
+    try {
 
-    // get users gravatar
+      // See if user exists
 
-    // encrypt password
+      // Get users gravatar
 
-    // return jsonwebtoken
+      // Encrypt password
 
-    res.send('User route');
+      // Return jsonwebtoken
+
+      res.send('User route');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+
+
+
+
+
   }
 );
 
