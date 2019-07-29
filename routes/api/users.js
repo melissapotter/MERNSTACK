a7e3dcf8611cc8744d525b594b649dceae95a0e4
melissapotter <melissapotter15@gmail.com>
2019-07-28 23:26:26 -0400

    added gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -27,14 +29,35 @@ router.post('/', [
     try {
 
       // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
 
       // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
 
       // Return jsonwebtoken
 
-      res.send('User route');
+      res.send('User registered');
 
     } catch (err) {
       console.error(err.message);
